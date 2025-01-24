c60d0364a393a9441f4dd9931c2851ffb7ae1312
Martin de La Gorce <madelago@microsoft.com>
2025-01-24 22:01:07 +0000

    Update __main__.py

--- fastermypy/__main__.py
+++ fastermypy/__main__.py
@@ -5,9 +5,10 @@ from pathlib import Path
 import sys
 from mypy import api
 import time
+from typing import Optional
 
 
-def load_config():
+def find_config(repo_root) -> Optional[Path]:
     """Load fastermypy settings from pyproject.toml."""
     current_dir = Path.cwd()
     while current_dir != current_dir.parent:
@@ -15,36 +16,28 @@ def load_config():
         if config_file.exists():
             return config_file
         current_dir = current_dir.parent
-        if (current_dir / ".git").exists():
+        if current_dir == repo_root:
             break
-    return {}
+    return None
 
 
-def find_mypy_config():
+def find_mypy_config(repo_root) -> Optional[Path]:
     """Search for a mypy.ini file up to the Git root directory."""
-    try:
-        git_root = subprocess.run(
-            ["git", "rev-parse", "--show-toplevel"],
-            text=True,
-            capture_output=True,
-            check=True,
-        ).stdout.strip()
-        git_root_path = Path(git_root)
-    except subprocess.CalledProcessError:
-        git_root_path = Path.cwd().root
 
     current_dir = Path.cwd()
-    while current_dir != git_root_path and current_dir != current_dir.parent:
+    while current_dir != repo_root and current_dir != current_dir.parent:
         config_path = current_dir / "mypy.ini"
         if config_path.exists():
             return config_path
+        if current_dir == repo_root:
+            break
         current_dir = current_dir.parent
 
-    config_path = git_root_path / "mypy.ini"
+    config_path = repo_root / "mypy.ini"
     return config_path if config_path.exists() else None
 
 
-def get_git_branch():
+def get_git_branch() -> str:
     """Get the current Git branch name."""
     try:
         result = subprocess.run(
@@ -58,44 +51,49 @@ def get_git_branch():
         return "default_branch"
 
 
-def get_repo_root():
+def get_repo_root() -> Path:
     """Get the root directory of the Git repository."""
-    try:
-        result = subprocess.run(
-            ["git", "rev-parse", "--show-toplevel"],
-            text=True,
-            capture_output=True,
-            check=True,
-        )
-        return Path(result.stdout.strip()).absolute()
-    except subprocess.CalledProcessError:
-        return Path.cwd().root
+
+    result = subprocess.run(
+        ["git", "rev-parse", "--show-toplevel"],
+        text=True,
+        capture_output=True,
+        check=True,
+    )
+    return Path(result.stdout.strip()).absolute()
 
 
-def run_mypy():
+def run_mypy() -> None:
     """Run mypy with branch-specific caching and optional pre-command."""
-    config_file = load_config()
-    print(f"Using fastmypy configurations from {config_file}")
-    config_data = toml.load(config_file)
-    config = config_data.get("tool", {}).get("fastermypy", {})
     branch_name = get_git_branch()
     repo_root = get_repo_root()
+    config_file = find_config(repo_root)
+    if config_file:
+        print(f"Using fastmypy configurations from {config_file}")
+        config_data = toml.load(config_file)
+        config = config_data.get("tool", {}).get("fastermypy", {})
+    else:
+        config = None
     cache_dir = config.get("cache_dir", "{repo_root}/.mypy_cache/branch/{branch_name}")
     assert isinstance(cache_dir, str)
     cache_dir = cache_dir.format(branch_name=branch_name, repo_root=repo_root)
     os.makedirs(cache_dir, exist_ok=True)
     print(f"Using cache directory: {cache_dir}")
 
-    pre_command = config.get("pre_command")
-    if pre_command:
-        start = time.time()
-        print(f"Running pre-command: {pre_command}")
-        # runn command and stop if failed
-        out = subprocess.check_call(pre_command, shell=True)
-        if out != 0:
-            raise Exception("Pre-command failed")
-        pre_command_duration = time.time() - start
-    config_file = find_mypy_config()
+
+    if config:
+        pre_command = config.get("pre_command")
+        if pre_command:
+            start = time.time()
+            print(f"Running pre-command: {pre_command}")
+            # runn command and stop if failed
+            out = subprocess.check_call(pre_command, shell=True)
+            if out != 0:
+                raise Exception("Pre-command failed")
+            pre_command_duration = time.time() - start
+            print(f"Pre-command took {pre_command_duration:3g} seconds")
+
+    mypy_config_file = find_mypy_config(repo_root)
 
     # Path(cache_dir).mkdir(exist_ok=True)
 
@@ -105,25 +103,23 @@ def run_mypy():
     # Build Mypy arguments list
     args = [f"--cache-dir={cache_dir}"]
     if config_file:
-        print(f"Using mypy configuration file: {config_file}")
-        args.append(f"--config-file={config_file}")
+        print(f"Using mypy configuration file: {mypy_config_file}")
+        args.append(f"--config-file={mypy_config_file}")
     args.extend(mypy_args)
 
-    print("Running Mypy")
+    print("Running Mypy...")
     start = time.time()
 
     # Run mypy via its API
     stdout, stderr, exit_status = api.run(args)
 
     end = time.time()
-
+    stdout= stdout.strip()
     if stdout:
         print(stdout)
     if stderr:
         print(stderr, file=sys.stderr)
 
-    if pre_command:
-        print(f"Pre-command took {pre_command_duration:3g} seconds")
     print(f"Mypy took {end-start:3g} seconds")
     sys.exit(exit_status)
 
