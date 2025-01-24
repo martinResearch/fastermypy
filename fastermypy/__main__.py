53fc37187f95d3452bf5f3cf70830f957be4b3c4
Martin de La Gorce <madelago@microsoft.com>
2025-01-24 22:30:37 +0000

    add possibility to use environment variables in the cache_dir

--- fastermypy/__main__.py
+++ fastermypy/__main__.py
@@ -76,7 +76,8 @@ def run_mypy() -> None:
         config = None
     cache_dir = config.get("cache_dir", "{repo_root}/.mypy_cache/branch/{branch_name}")
     assert isinstance(cache_dir, str)
-    cache_dir = cache_dir.format(branch_name=branch_name, repo_root=repo_root)
+    vaiables = {**os.environ, "branch_name": branch_name, "repo_root": repo_root}
+    cache_dir = cache_dir.format(vaiables)
     os.makedirs(cache_dir, exist_ok=True)
     print(f"Using cache directory: {cache_dir}")
 
